25c761bfb467810d92af6a7e259ba9c866e1378f
Juraj Marcin <juraj@jurajmarcin.com>
2018-05-05 11:42:33 +0200

    Update

---
+++ MailSender/Config.cs
@@ -0,0 +1,46 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.IO;
+
+namespace MailSender {
+	class Config {
+		public static string CsvDbPath { get; private set; }
+
+		public static string SmtpServer { get; private set; }
+		public static int SmtpPort { get; private set; }
+		public static bool SmtpSsl { get; private set; }
+
+		public static string MailAccount { get; private set; }
+		public static string MailPasswd { get; private set; }
+		public static string MailDisplayName { get; private set; }
+
+		public static string MailHtmlContent { get; private set; }
+		public static string MailPlainContent { get; private set; }
+		public static string MailSubject { get; private set; }
+		public static string MailPhotoName { get; private set; }
+
+		static Config() {
+			JToken token = JObject.Parse(File.ReadAllText("config.json"));
+
+			CsvDbPath = (string)token.SelectToken("CsvDbPath");
+
+			SmtpServer = (string)token.SelectToken("SmtpServer");
+			SmtpPort = (int)token.SelectToken("SmtpPort");
+			SmtpSsl = (bool)token.SelectToken("SmtpSsl");
+
+			MailAccount = (string)token.SelectToken("MailAccount");
+			MailPasswd = (string)token.SelectToken("MailPasswd");
+			MailDisplayName = (string)token.SelectToken("MailDisplayName");
+			MailPhotoName = (string)token.SelectToken("MailPhotoName");
+
+			try {
+				MailHtmlContent = File.ReadAllText((string)token.SelectToken("MailHtmlContentFile"));
+				MailPlainContent = File.ReadAllText((string)token.SelectToken("MailPlainContentFile"));
+			} catch {
+				Console.WriteLine(@"Invalid path set for mail body. SET IT UP!");
+			}
+
+			MailSubject = (string)token.SelectToken("MailSubject");
+		}
+	}
+}
---
+++ MailSender/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Mail;
+using System.Net;
+using System.Net.Mime;
+
+namespace MailSender {
+	class Program {
+
+		private static MailAddress _senderAddress;
+		private static SmtpClient _smtpClient;
+
+		static void Main(string[] args) {
+			_senderAddress = new MailAddress("columba@gympos.sk", "Columba Racing");
+			_smtpClient = new SmtpClient("smtp.gympos.sk", 465) {
+				EnableSsl = true,
+				UseDefaultCredentials = false,
+				Credentials = new NetworkCredential("columba", "4X4/pse"),
+				DeliveryMethod = SmtpDeliveryMethod.Network
+			};
+			
+			var reciever = new MailAddress(args[0]);
+			
+			var mail = new MailMessage(_senderAddress, reciever)
+			{
+				HeadersEncoding = Encoding.UTF8,
+				SubjectEncoding = Encoding.UTF8,
+				BodyEncoding = Encoding.UTF8,
+				Subject = Config.MailSubject,
+			};
+
+			var pic = new Attachment(args[1], new ContentType("image/jpeg"))
+			{
+				Name = Config.MailPhotoName,
+				ContentDisposition = {Inline = true}
+			};
+
+			var plainTextView = AlternateView.CreateAlternateViewFromString(Config.MailPlainContent, Encoding.UTF8,
+				MediaTypeNames.Text.Plain);
+			var htmlView = AlternateView.CreateAlternateViewFromString(Config.MailHtmlContent, Encoding.UTF8,
+				MediaTypeNames.Text.Html);
+
+			var inline = new LinkedResource(args[1], MediaTypeNames.Image.Jpeg) {ContentId = "photo"};
+			htmlView.LinkedResources.Add(inline);
+
+			mail.AlternateViews.Add(plainTextView);
+			mail.AlternateViews.Add(htmlView);
+
+			mail.Attachments.Add(pic);
+
+			_smtpClient.Send(mail);
+		}
+	}
+}
